542f937b43cf1aed957d30c7cdf986922d7a60d9
vega <stanislasvega@yahoo.fr>
2020-01-22 17:55:37 +0100

    remove background login image

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+import authProvider from './authProvider';
 import { API_URL } from './api-config';
 import { PartnersList, PartnersEdit, PartnersCreate,PostIcon } from './partners';
 import { RessourcesList, RessourcesEdit, RessourcesCreate } from './ressources';
 
 const App = () => (
   <div className="App">
-    <Admin dataProvider={jsonServerProvider(API_URL)}>
+    <Admin  authProvider={authProvider} dataProvider={jsonServerProvider(API_URL)}>
         <Resource name="partners" list={PartnersList} edit={PartnersEdit} create={PartnersCreate} icon={PostIcon}/>
         <Resource name="ressources" list={RessourcesList} edit={RessourcesEdit} create={RessourcesCreate} icon={PostIcon}/>
     </Admin>
