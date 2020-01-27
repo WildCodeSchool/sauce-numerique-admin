f3659ae95b7aa47fc8c132f3a8edb23e1828b922
vega <stanislasvega@yahoo.fr>
2020-01-27 16:29:44 +0100

    add search bar

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import authProvider from './authProvider';
 import { API_URL } from './api-config';
+import GroupWorkIcon from '@material-ui/icons/GroupWork';
 import { PartnersList, PartnersEdit, PartnersCreate,PostIcon } from './partners';
 import { RessourcesList, RessourcesEdit, RessourcesCreate } from './ressources';
 
 const App = () => (
   <div className="App">
     <Admin  authProvider={authProvider} dataProvider={jsonServerProvider(API_URL)}>
-        <Resource name="partners" list={PartnersList} edit={PartnersEdit} create={PartnersCreate} icon={PostIcon}/>
+        <Resource name="partners" list={PartnersList} edit={PartnersEdit} create={PartnersCreate} icon={GroupWorkIcon}/>
         <Resource name="ressources" list={RessourcesList} edit={RessourcesEdit} create={RessourcesCreate} icon={PostIcon}/>
     </Admin>
   </div>
--- src/ressources.js
+++ src/ressources.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import { List, Datagrid, Edit, Create, SimpleForm, DateField, TextField, EditButton, TextInput, DateInput } from 'react-admin';
+import { List, Datagrid, Edit, Create, SimpleForm, DateField, TextField, EditButton, TextInput, DateInput,Filter, Pagination } from 'react-admin';
 import BookIcon from '@material-ui/icons/Book';
 export const PostIcon = BookIcon;
 
+const UsersPagination = (props) => (
+    <Pagination
+    rowsPerPageOptions= {
+        [
+            5, 10, 25, 50, 100
+        ]
+    }{...props}
+    />
+)
+
+const RessourcesFilter = (props) => (
+    <Filter {...props}>
+        <TextInput label="Search" source="q" alwaysOn />
+    </Filter>
+);
 export const RessourcesList = (props) => (
-    <List {...props}>
+    <List filters={<RessourcesFilter />} {...props} pagination={<UsersPagination />}>
         <Datagrid>
             <TextField source="id" />
             <TextField source="title" />
@@ -37,7 +52,7 @@ export const RessourcesEdit = (props) => (
 );
 
 export const RessourcesCreate = (props) => (
-    <Create title="Create a Post" {...props}>
+    <Create title="Create a ressource" {...props}>
         <SimpleForm>
             <TextInput source="title" />
             <TextInput source="theme" />
