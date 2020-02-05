99a08d0a12b5472af64db779a99e044b5d1fb2de
vega <stanislasvega@yahoo.fr>
2020-02-05 11:34:20 +0100

    change varialble react-app

--- src/api-config.js
+++ src/api-config.js
@@ -1,4 +1,4 @@
 
-const backendHost = process.env.PORT || '';
+const backendHost = process.env.REACT_APP_PORT || '';
 // console.log(process.env.REACT_APP_BACKEND_HOST)
 export const API_URL = `${backendHost}/api/v1`;
